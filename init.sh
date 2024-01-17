43124519c2e9820ca1071260978c90d37c9a6ecf
kissshot <4530897+kkkgo@users.noreply.github.com>
2024-01-18 01:50:45 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@ if [ -z "$DNS" ]; then
     echo "DNS not set, use default DNS."
 else
     echo "Apply DNS: ""$DNS"
-    echo "$DNS" | grep -Eo "[^,]+" | sed "s/^/nameserver /g" >/etc/resolv.conf
+    echo "$DNS" | sed 's/"//g' | grep -Eo "[^,]+" | sed "s/^/nameserver /g" >/etc/resolv.conf
 fi
 sed 's/\r$//' /data/caddyfile.txt >/tmp/caddyfile
 caddy run --config /tmp/caddyfile --adapter caddyfile
